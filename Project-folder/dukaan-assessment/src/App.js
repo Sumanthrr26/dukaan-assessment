9582c488d79e84ce801f1dfee659042c4a7c25b0
Sumanthrr26 <Sumanthr26@gmail.com>
2023-12-23 10:37:15 +0530

    Final push

---
+++ Project-folder/dukaan-assessment/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Project-folder/dukaan-assessment/src/Components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./sidebar.css";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-btn">
+        <button> Home</button>
+        <button> Home</button>
+        <button> Home</button>
+        <button> Home</button>
+        <button> Home</button>
+        <button> Home</button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
